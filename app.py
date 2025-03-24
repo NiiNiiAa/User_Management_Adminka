3b60a3b9d88545e5ca176fbc45e046795c60ab0b
Naniko Tsertsvadze <n.tsertsvadze@crystal.ge>
2025-03-24 16:19:08 +0400

    Sign Up Page Changes made

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
 from flask import Flask, render_template
 
 
-app = app = Flask(__name__)
+app = Flask(__name__)
 
 @app.route("/")
-@app.route("/home")
 def home():
     return render_template("base.html")
 
 @app.route("/login")
 def login():
-    return render_template("auth.html")
+    return render_template("login.html")
 
-@app.route("/signup")
-def signup():
-    return render_template("signup.html")
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
